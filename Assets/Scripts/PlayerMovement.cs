260943f7d3879fcf700100826200a063a02a10f0
Aditi Verma <anvil557@gmail.com>
2025-04-03 15:10:53 +0530

    terrain and trees

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,43 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{   
-    [SerializeField] private float moveSpeed = 5f;
-    public Rigidbody rb;
+public class PlayerMovementRB : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+    public float jumpForce = 5f;
+    public float groundCheckDistance = 0.2f;
+    public LayerMask groundMask;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (transform.position.y < 0) // If player falls below terrain
+        {
+            transform.position = new Vector3(transform.position.x, 5, transform.position.z); // Reset above ground
+        }
+        // Check if Player is Grounded
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundMask);
+
+        // Get Input
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        rb.AddForce(new Vector3(x *  moveSpeed, rb.linearVelocity.y,z *  moveSpeed));
+
+        // Move Player
+        Vector3 move = transform.right * x + transform.forward * z;
+        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+        rb.linearVelocity = new Vector3(move.x * speed, rb.linearVelocity.y, move.z * speed);
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+        }
     }
 }
